0919ee5d005762edc02b0c67d89607f0331aa63e
Jhy1993 <823322482@qq.com>
2016-11-06 19:47:08 +0800

    add some word embedding

---
+++ embedding_partitioned.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 06 19:45:58 2016
+
+@author: Jhy1993
+Github: https://github.com/Jhy1993
+
+README:
+
+Reference:
+https://github.com/eliben/deep-learning-samples/blob/master/tensorflow-samples/embedding_partitioned.py
+"""
+
